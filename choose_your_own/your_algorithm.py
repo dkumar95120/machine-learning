5f98db3522479bdf007185a7bef5e7d1c543351f
dkumar95120 <dkumar@gmail.com>
2017-02-07 15:58:41 -0800

    Classifier Comparison

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -31,11 +31,52 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+from time import time
+import numpy as np
 
+from matplotlib.colors import ListedColormap
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_moons, make_circles, make_classification
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Gaussian Process",
+         "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
+         "Naive Bayes", "QDA"]
+rng = np.random.RandomState(1)
+# initialize classifier with chosen parameters
+classifiers = [
+    KNeighborsClassifier(3, weights='distance'),
+    SVC(kernel="linear", C=0.025),
+    SVC(gamma=2, C=1),
+    GaussianProcessClassifier(1.0 * RBF(1.0), warm_start=True),
+    DecisionTreeClassifier(max_depth=5),
+    RandomForestClassifier(max_depth=None, n_estimators=10, min_samples_split=2, random_state=0, max_features=1),
+    MLPClassifier(alpha=1),
+    AdaBoostClassifier(random_state=rng),
+    GaussianNB(),
+    QuadraticDiscriminantAnalysis()]
 
-
-
-
+for name, clf in zip(names, classifiers):
+	# train classfier with training data and measure time taken to train the classifier
+	t0 = time()
+	clf = clf.fit(features_train, labels_train)
+	print name, "training time:", round(time()-t0, 3),"s"
+	# now make predictions based on the test data and measure time taken to predict
+	t0 = time()
+	labels_pred = clf.predict(features_test)
+	print name, "prediction time:", round(time()-t0, 3),"s"
+	# find accuracy of the predictions as compared to the actual results in the test data
+	from sklearn.metrics import accuracy_score 
+	score = accuracy_score(labels_test, labels_pred)
+	print name, "accuracy = ", score
 
 
 try:
